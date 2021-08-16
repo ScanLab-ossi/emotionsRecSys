a34a28b7c48ce393c18c5ed49553c43a97a3bc24
LLansman <58682786+LLansman@users.noreply.github.com>
2021-08-16 11:19:28 +0300

    Add files via upload

--- src/content/Preferences/movieSidePanel.jsx
+++ src/content/Preferences/movieSidePanel.jsx
@@ -1,17 +1,23 @@
 import ReactStars from "react-rating-stars-component";
 import React, {Component} from "react";
 
+
 class MovieSidePanel extends Component {
 	constructor(props) {
 		super(props);
 	
 		this.state = {
 		 
-		  selected: false
+		  selected: false,
+		  liColor: 'white'
 		};
 	  }
 	
-
+	liStyle=()=>{
+		this.setState({
+			liColor: '#8fd6f2'
+        })
+	}
 
 	
 
@@ -34,17 +40,23 @@ class MovieSidePanel extends Component {
 							
 							{this.props.movieList.map((movie) => (
 
-								<li key={movie.movie_id } 
-									style={{cursor: 'pointer'}}
+								<li key={movie.movie_id}  
+									style={{cursor: 'pointer', background:this.state.liColor}}
 									className="list-group-item d-flex justify-content-between align-items-center"
 									onClick={() => this.props.handler(true, movie)}
 									onMouseLeave={() => this.props.handler(false, movie)}	
+
+
+									//TODO onMouseLeave={() =>  this.setState({	liColor: 'white'})}	
+									//TODO onMouseEnter={() => this.setState({	liColor: '#8fd6f2'})} 
 									
 																	
 								>
 						
 									<b> {movie.title} </b>
-																
+									<img src={movie.poster}  alt="..." height="40px" width="40px" />
+
+															
 								</li>
 							))}
 						</ol>
